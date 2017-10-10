4bed30b09f9515c0790a8aaa1935f9ae682adb31
RoflCopter4 <brendan.leason.4@gmail.com>
2017-10-10 01:59:40 -0600

    y u no load

---
+++ myMolokai-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; myMolokai-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "myMolokai-theme" "myMolokai-theme.el" (22912 15440
+;;;;;;  307143 101000))
+;;; Generated autoloads from myMolokai-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; myMolokai-theme-autoloads.el ends here
